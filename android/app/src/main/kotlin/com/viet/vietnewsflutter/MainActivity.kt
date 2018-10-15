a559e936250a8df6289049b449838a529ebf8f67
L <554966574@qq.com>
2018-10-15 19:47:45 +0800

    Android 状态栏透明

--- android/app/src/main/kotlin/com/viet/vietnewsflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/viet/vietnewsflutter/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.viet.vietnewsflutter
 
 import android.os.Bundle
-
+import android.os.Build
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity(): FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity() : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//API>21,设置状态栏颜色透明
+            getWindow().setStatusBarColor(0)
+        }
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
