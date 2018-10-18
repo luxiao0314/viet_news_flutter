e5e40442f87f568b0bdfc1db6044f5e0d7d52a1a
xiao.lu <xiao.lu@magicwindow.cn>
2018-10-18 10:53:48 +0800

    Update

--- lib/http/HttpInterceptor.dart
+++ lib/http/HttpInterceptor.dart
@@ -20,11 +20,11 @@ class HttpInterceptor {
     };
     dio.interceptor.response.onSuccess = (Response response) async {
       print("response: $response");
-      if (json.decode(response.data)["code"] == 0) {
-        return response.data;
-      } else {
-        toasts(json.decode(response.data)["message"]);
-      }
+//      if (json.decode(response.data)["code"] == 0) {
+//        return response.data;
+//      } else {
+//        toasts(json.decode(response.data)["message"]);
+//      }
       return response;
     };
     dio.interceptor.response.onError = (DioError e) async {
--- lib/http/fetch.dart
+++ lib/http/fetch.dart
@@ -15,7 +15,7 @@ class Fetch {
     if (instance == null) {
       instance = new Fetch();
     }
-    return new Fetch();
+    return instance;
   }
 
   Fetch() {
@@ -26,17 +26,32 @@ class Fetch {
         receiveTimeout: 5000,
         headers: {
           "Authorization":
-              "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdXN0b20iLCJwaG9uZU51bWJlciI6IjE4Njc0MzU1MDQxIiwicm9sZUlkIjoiMSIsImlzcyI6Im1lcmN1bGV0IiwiZXhwIjoxNTQyMzU4MTA5LCJ1c2VySWQiOiIyMCIsImlhdCI6MTUzOTc2NjEwOX0.bLA3XwTMUqz3AqKCjLUbam3ChZeXNLWPrcM6pHe7r8Q",
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdXN0b20iLCJwaG9uZU51bWJlciI6IjE4Njc0MzU1MDQxIiwicm9sZUlkIjoiMSIsImlzcyI6Im1lcmN1bGV0IiwiZXhwIjoxNTQyMzU4MTA5LCJ1c2VySWQiOiIyMCIsImlhdCI6MTUzOTc2NjEwOX0.bLA3XwTMUqz3AqKCjLUbam3ChZeXNLWPrcM6pHe7r8Q",
         });
     dio = new Dio(options);
     HttpInterceptor(dio);
   }
 
   Future<dynamic> get(String path, {dynamic data}) async {
-    return dio.get(path, data: data);
+    return dio.get(path, data: data).then(_checkStatus);
   }
 
   Future<dynamic> post(String path, {dynamic data}) async {
-    return dio.post(path, data: data);
+    return dio.post(path, data: data).then(_checkStatus);
+  }
+
+  Future<dynamic> _checkStatus(Response response) async {
+    // 如果http状态码正常，则直接返回数据
+    if (response.statusCode == HttpStatus.ok) {
+      if (json.decode(response.data)["code"] == 0) {
+        return response.data;
+      }
+      throw DioError(response: response,
+          message: json.decode(response.data)["message"],
+          type: DioErrorType.RESPONSE);
+    } else {
+      throw DioError(
+          response: response, message: "网络异常", type: DioErrorType.RESPONSE);
+    }
   }
 }
