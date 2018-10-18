79f9c2fc6a39dec3aa0215bb25ceddb98cc1f450
Zongjia Long <zongjia.long@merculet.io>
2018-10-18 16:27:49 +0800

    多语言切换

--- lib/page/mine/LanguageSettingPage.dart
+++ lib/page/mine/LanguageSettingPage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 import 'package:viet_news_flutter/local/Local.dart';
+import 'package:viet_news_flutter/redux/LocalRedux.dart';
+import 'package:viet_news_flutter/redux/NewsState.dart';
 import 'package:viet_news_flutter/res/colors.dart';
 import 'package:viet_news_flutter/view/SelectItem.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -37,16 +41,38 @@ class _LanguageSettingPage extends State<LanguageSettingPage> {
         backgroundColor: bg_white,
         elevation: 0.0,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return SelectItem(
-            label: type[index],
-            isSelected: true,
-            onPress: () async {},
+      body: StoreBuilder<NewsState>(
+        builder: (context, store) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return SelectItem(
+                label: type[index],
+                isSelected: true,
+                onPress: () async {
+                  changeLocale(store, index);
+                  Navigator.pop(context);
+                },
+              );
+            },
+            itemCount: 3,
           );
         },
-        itemCount: 3,
       ),
     );
+  }
+
+  ///切换语言
+  static changeLocale(Store<NewsState> store, int index) {
+    Locale locale = store.state.platformLocale;
+    switch (index) {
+      case 0:
+      case 1:
+        locale = Locale('zh', 'CH');
+        break;
+      case 2:
+        locale = Locale('en', 'US');
+        break;
+    }
+    store.dispatch(RefreshLocaleAction(locale));
   }
 }
