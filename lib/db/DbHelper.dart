4aa14f5e2cf126936201f5216b77dfc0ef3498c7
Zhiqiang Liu <aaron@magicwindow.cn>
2018-10-22 21:21:20 +0800

    增加注释

--- lib/db/DbHelper.dart
+++ lib/db/DbHelper.dart
@@ -3,6 +3,10 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+/// @Author Aaron
+/// @Email aaron@merculet.io
+/// @Date 2018/10/22
+/// @Description 数据库操作辅助类
 class DbHelper {
   Database database;
 
@@ -26,19 +30,22 @@ class DbHelper {
 
   //插入
   Future<int> insert(String itemId, String dataValue) async {
-      return await database
-          .insert(TABLE_NAME, {"$ITEM_ID": itemId, "$DATA": dataValue});
+    return await database
+        .insert(TABLE_NAME, {"$ITEM_ID": itemId, "$DATA": dataValue});
   }
+
   //更新(如果原来没有此条数据，不会插入)
   Future<int> update(String itemId, String dataValue) async {
     return await database
         .update(TABLE_NAME, {"$ITEM_ID": itemId, "$DATA": dataValue});
   }
+
   //替换(如果原来没有此条数据，则插入)
   Future<int> replace(String itemId, String dataValue) async {
-    return await database
-        .execute("REPLACE INTO $TABLE_NAME($ITEM_ID, $DATA) VALUES ('$itemId','$dataValue')");
+    return await database.execute(
+        "REPLACE INTO $TABLE_NAME($ITEM_ID, $DATA) VALUES ('$itemId','$dataValue')");
   }
+
   //查询
   Future<List<Map<String, dynamic>>> query({String itemId}) async {
     if (itemId != null) {
@@ -50,11 +57,10 @@ class DbHelper {
 
   //删除
   Future<int> delete({String itemId}) async {
-    if(itemId!=null){
+    if (itemId != null) {
       print("itemId1:$itemId");
       return await database.delete(TABLE_NAME, where: "$ITEM_ID = $itemId");
-
-    } else{
+    } else {
       print("itemId2:$itemId");
       return await database.delete(TABLE_NAME);
     }
@@ -90,6 +96,5 @@ class DbHelper {
     }
 
     database = await openDatabase(path, version: 1, onCreate: _onCreate);
-    print("datebase$database");
   }
 }
